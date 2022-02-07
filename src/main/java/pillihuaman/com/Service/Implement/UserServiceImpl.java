d917371dbc63dbd34725145fd62468194a865533
zarmir <pillihuamanhz@gmail.com>
2022-02-07 17:35:24 -0500

    init bd

--- src/main/java/pillihuaman/com/Service/Implement/UserServiceImpl.java
+++ src/main/java/pillihuaman/com/Service/Implement/UserServiceImpl.java
@@ -1,31 +1,47 @@
 package pillihuaman.com.Service.Implement;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.function.Consumer;
 
+import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 
-import pillihuaman.com.BusinessEntity.model.Color;
+import com.mongodb.Block;
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import pillihuaman.com.BusinessEntity.model.AuditEntity;
 import pillihuaman.com.BusinessEntity.model.User;
 import pillihuaman.com.Help.ConvertClass;
-import pillihuaman.com.Help.RegisterStatus;
 import pillihuaman.com.Repository.UserRepository;
-import pillihuaman.com.Service.ColorService;
 import pillihuaman.com.Service.UserService;
+import pillihuaman.com.basebd.user.domain.dao.UserProcessRepository;
+import pillihuaman.com.model.request.ReqUser;
 import pillihuaman.com.model.response.RespBase;
-import pillihuaman.com.model.response.RespColor;
 import pillihuaman.com.model.response.RespUser;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Component
 public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired(required=false)
+	private UserProcessRepository userProcessRepository;
 
-	@Autowired
-	private ColorService colorService;
+
 
 	@Override
 	public RespBase<RespUser> getUserByMail(String mail) {
@@ -34,7 +50,7 @@ public class UserServiceImpl implements UserService {
 		User filtro = new User();
 
 		filtro.setMail(mail);
-		filtro.setStatus(RegisterStatus.ACTIVE.getCode());
+		//filtro.setStatus(RegisterStatus.ACTIVE.getCode());
 		Example<User> example = Example.of(filtro);
 		List<User> lista = userRepository.findAll(example);
 		if (lista != null) {
@@ -57,7 +73,7 @@ public class UserServiceImpl implements UserService {
 			User filtro = new User();
 
 			filtro.setUsername(username);
-			filtro.setStatus(RegisterStatus.ACTIVE.getCode());
+			//filtro.setStatus(RegisterStatus.ACTIVE.getCode());
 			Example<User> example = Example.of(filtro);
 			List<User> lista = new ArrayList<>();
 			lista = userRepository.findAll(example);
@@ -72,5 +88,57 @@ public class UserServiceImpl implements UserService {
 		}
 		return respo;
 	}
+	
+	@Override
+	public RespBase<RespUser> registerUser(ReqUser request) {
+		MongoClient clin = MongoClients.create("mongodb://localhost:27017");
+		MongoTemplate template= new MongoTemplate(clin,"gamachicas");
+		 /* public @Bean MongoClient mongoClient() {
+		      return MongoClients.create("mongodb://localhost:27017");
+		  }
 
+		  public @Bean MongoTemplate mongoTemplate() {
+		      return new MongoTemplate(mongoClient(), "gamachicas");
+		  }*/
+		  
+		try {
+		RespBase<RespUser> respo = new RespBase<RespUser>();
+		pillihuaman.com.basebd.user.domain.User filtro = new pillihuaman.com.basebd.user.domain.User();
+		filtro.setId(new ObjectId());
+		filtro.setAlias("");
+		filtro.setApiPassword("");
+		filtro.setIdSystem( 1);
+		filtro.setIdUser(2);
+		filtro.setMail(request.getMail());
+		filtro.setMobilPhone("933418411");
+		filtro.setPassword("933418411");
+		filtro.setSalPassword("933418411");
+		filtro.setUsername("zarmir");
+		AuditEntity auditEntity= new AuditEntity();
+		auditEntity.setCodUsuModif("o1Zarmir");
+		auditEntity.setCodUsuRegis("o1Zarmir");
+		auditEntity.setFecModif(new Date());
+		auditEntity.setFecRegis(new Date());
+		filtro.setAuditEntity(auditEntity);
+		userProcessRepository.insert(filtro);
+		List<pillihuaman.com.basebd.user.domain.User> lis=userProcessRepository.findUserByMail("pillihuamanhz@gmail.com");
+		pillihuaman.com.basebd.user.domain.User filtroM = new pillihuaman.com.basebd.user.domain.User();		
+		
+		//Query query = new Query();
+		//query.addCriteria(Criteria.where("id").is("pillihuamanhz@gmail.com"));
+		//query.
+
+
+        
+		//List<pillihuaman.com.basebd.user.domain.User> users = template.find(query, pillihuaman.com.basebd.user.domain.User.class);
+		
+		filtroM.setMail("pillihuamanhz@gmail.com");
+		Example<pillihuaman.com.basebd.user.domain.User> example = Example.of(filtroM);
+		//userProcessRepository.findAll(sort);
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+		return null;
+	}
+	    
 }
