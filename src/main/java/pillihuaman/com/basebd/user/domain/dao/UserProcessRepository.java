2b2161677954cec1a8abc89debb486d0296420c2
zarmir <pillihuamanhz@gmail.com>
2022-02-25 11:25:00 -0500

    cambio

--- src/main/java/pillihuaman/com/basebd/user/domain/dao/UserProcessRepository.java
+++ src/main/java/pillihuaman/com/basebd/user/domain/dao/UserProcessRepository.java
@@ -14,7 +14,11 @@ public interface  UserProcessRepository extends MongoRepository<User, String>  {
 	
 	 @Query("{ 'mail' : ?0 }")
 	  List<User> findUserByMail(String mail);
+<<<<<<< HEAD
+
+=======
 	 
 	 @Query("{ 'username' : ?0 }")
 	  List<User> findUserName(String mail);
+>>>>>>> 62b9c0006d6dcd4cc7c651a55c0ce0c68c46e109
 }
